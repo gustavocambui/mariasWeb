078c79385d4baa0e5fad5693dc86cb84fdc0295b
nathaliacrz <nathalias161@gmail.com>
2022-08-15 20:07:05 -0300

    footer visivel apenas ao logar

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -4,40 +4,53 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import { Typography, Grid } from '@material-ui/core';
 import { Box } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
 
 function Footer() {
+
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
+
+    var footerComponent;
+
+    if (token != "") {
+        footerComponent = <Grid container direction="row" justifyContent="center" alignItems="center">
+        <Grid alignItems="center" item xs={12}>
+            <Box style={{backgroundColor: "#5d2019", height: "120px" }}>
+                <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                    <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                </Box>
+                <Box display="flex" alignItems="center" justifyContent="center">
+                    <a href="https://www.facebook.com/Marias-Web-111470941659330/?ref=page_internal" target="_blank">
+                        <FacebookIcon style={{ fontSize: 60, color: "white" }} />
+                    </a>
+                    <a href="https://www.instagram.com/marias_web/" target="_blank">
+                        <InstagramIcon style={{ fontSize: 60, color: "white" }} />
+                    </a>
+                    <a href="https://github.com/gustavocambui/mariasWeb" target="_blank">
+                        <GitHubIcon style={{ fontSize: 60, color: "white" }} />
+                    </a>
+                </Box>
+            </Box>
+            <Box style={{ backgroundColor: "#5d2019", height: "60px" }}>
+                <Box paddingTop={1}>
+                    <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
+                </Box>
+                <Box>
+                    <a target="_blank" href="https://brasil.generation.org">
+                        <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
+                    </a>
+                </Box>
+            </Box>
+        </Grid>
+    </Grid>
+    }
+
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box style={{backgroundColor: "#5d2019", height: "120px" }}>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/Marias-Web-111470941659330/?ref=page_internal" target="_blank">
-                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                            <a href="https://www.instagram.com/marias_web/" target="_blank">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                            <a href="https://github.com/gustavocambui/mariasWeb" target="_blank">
-                                <GitHubIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box style={{ backgroundColor: "#5d2019", height: "60px" }}>
-                        <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
-                        </Box>
-                        <Box>
-                            <a target="_blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
-                            </a>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
+            {footerComponent}
         </>
     )
 }
